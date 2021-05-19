e38e01bb284fcfdcad14be6ca75076a9b2a3d435
samia-cherni <samiacherni@outlook.com>
2021-05-20 00:47:11 +0100

    saving changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from "react";
 import Profile from "./profile/Profile";
 
 function App() {
-  function handleName(name) {
-    alert(`${name}`);
-  }
+  const handleName = name => alert(`${name}`);
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-mycolor">
@@ -13,14 +11,14 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col">
-            <Profile></Profile>
+            <Profile handleName={handleName}></Profile>
           </div>
           <div className="col">
             <Profile
               fullName="Lilly Grey"
               bio="Lilly Grey is a working designer as well as a design trainer and author. Her design firm, provides design services for web, logo, and print."
               profession="Designer"
-              showName={handleName}
+              handleName={handleName}
             >
               <img src="1.jpg" className="card-img-top" alt="profile" />
             </Profile>
@@ -30,16 +28,16 @@ function App() {
               fullName="John Smith"
               bio="John Smith has been listed as one of Fast Company’s “Most Innovative Business People” and one of Fortune’s “40 under 40.” He is an early-stage technology investor/advisor."
               profession="Business man"
-              showName={handleName}
+              handleName={handleName}
             >
               <img src="2.jpg" className="card-img-top" alt="profile" />
             </Profile>
           </div>
         </div>
       </div>
-      <footer>
+      <div className='footer'>
         <p style={{textAlign:'center',fontWeight:'bold',color: 'grey'}}>&#169; 2021</p>
-      </footer>
+      </div>
     </div>
   );
 }
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Profile=(props)=>{
+const Profile=({fullName,profession,bio,handleName,children})=>{
     return(
-        <div className="card m-3" style={{width: '18rem'}} onClick={()=>props.showName(props.fullName)}>
+        <div className="card m-3" style={{width: '18rem'}} onClick={()=>handleName(fullName)}>
             <div className="card-body">
-            {props.children}
-            <h5 className="card-title"style={{fontWeight:'bold'}}>{props.fullName}</h5>
-                <p className="card-text">{props.bio}</p>
+            {children}
+            <h5 className="card-title"style={{fontWeight:'bold'}}>{fullName}</h5>
+                <p className="card-text">{bio}</p>
             </div>
             <ul className="list-group list-group-flush">
-                <li className="list-group-item">{props.profession}</li>
+                <li className="list-group-item">{profession}</li>
             </ul>
         </div>
     );
@@ -19,14 +19,13 @@ Profile.defaultProps={
     fullName : "Jane Roe",
     bio : "Jane Roe is the New York Times best-selling writer of Ancestors Of The Future and Trust The Shadows. The author or coauthor of ten books, Jane has sold 8 million copies since 2015.",
     profession : "Author",
-    children:<img src="/3.jpg" className="card-img-top" alt="profile" />,
-    showName : ()=>{alert('Jane Roe');}
+    children : <img src="/3.jpg" className="card-img-top" alt="profile" />
 };
 Profile.propTypes = {
     fullName : PropTypes.string.isRequired,
     bio : PropTypes.string.isRequired,
     profession : PropTypes.string.isRequired,
-    showName : PropTypes.func,
+    handleName : PropTypes.func,
     children : PropTypes.element.isRequired
 };
 export default Profile;
